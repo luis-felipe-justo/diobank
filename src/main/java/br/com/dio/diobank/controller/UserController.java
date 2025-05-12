487b38a5f4b40b66ae4708876c46f559449a60ac
ljusto <luis.justo@foursys.com.br>
2025-05-12 13:11:02 -0300

    finalização do projeto

---
+++ src/main/java/br/com/dio/diobank/controller/UserController.java
@@ -0,0 +1,33 @@
+package br.com.dio.diobank.controller;
+
+import br.com.dio.diobank.domain.model.User;
+import br.com.dio.diobank.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser (@RequestBody User user){
+        User userCreated = userService.createUser(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+}
